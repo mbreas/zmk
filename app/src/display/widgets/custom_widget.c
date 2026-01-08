2fa33a42ea3cf5c307354679bd186dbffe51d869
mbreas <marcel.breas@gmail.com>
2026-01-08 13:40:28 +0000

    not a word

--- app/src/display/widgets/custom_widget.c
+++ app/src/display/widgets/custom_widget.c
@@ -9,7 +9,7 @@
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include <zmk/display.h>
-#include <zmk/display/widgets/custom.h>
+#include <zmk/display/widgets/custom_widget.h>
 #include <zmk/events/layer_state_changed.h>
 #include <zmk/event_manager.h>
 #include <zmk/endpoints.h>
